aa90b5f41d5bf9e42ab2b78a36cbc914ee8ca093
Mario <marinaldo12@hotmail.com>
2025-10-07 16:12:17 -0300

    update do related name para plural

--- livros/urls.py
+++ livros/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import LivroListCreateAPIView, LivroDetail, homepage_view
+from .views import LivroListCreateAPIView, LivroDetail
 
 
 urlpatterns = [
     path("livros/", LivroListCreateAPIView.as_view(), name="livro-list-create"),
-    path("livros/<int:pk>/", LivroDetail.as_view(), name="livro_detail"),
-    path('', homepage_view, name='home')
+    path("livros/<int:pk>/", LivroDetail.as_view(), name="livro_detail")
 ]
