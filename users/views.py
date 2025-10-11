d140f48191634f34f1d93db1969b085a8415f681
Mario <marinaldo12@hotmail.com>
2025-10-11 13:52:09 -0300

    criação das view do user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from .models import User
+from .serializer import UserSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+class UserReadAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AllowAny]
+
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                user = User.objects.get(pk=pk)
+                serializer = UserSerializer(user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                return Response({"error":"Usuário não localizado."}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserCreateAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserUpdateAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request, pk):
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error":"Usuário não localizado."},status=status.HTTP_404_NOT_FOUND)
+        if request.user == user or request.user.is_superuser:
+            serializer = UserSerializer(instance=user, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error":"Usuário não tem permissão para alterar outro usuário."}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDestroyAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            if user == request.user or request.user.is_superuser:
+                user.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        except User.DoesNotExist:
+            return Response({"error":"Usuário não localizado."}, status=status.HTTP_404_NOT_FOUND)
+        
